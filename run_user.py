e938a89f4f1717f04262abf98cb78e61118f8a86
Alice Mwihaki <alicemwihaki99@gmail.com>
2019-12-16 13:49:37 +0300

    created functions to find contacts,it it exits and displays

--- run_user.py
+++ run_user.py
@@ -19,3 +19,24 @@ def del_user(user):
     Function to delete a user
     '''
     user.delete_user()
+    
+    
+def find_user(name):
+    '''
+    Function that finds a user by name and returns the user
+    '''
+    return User.find_by_name(name)
+
+
+def check_existing_user(name):
+    '''
+    Function that check if a user exists with that name and return a Boolean
+    '''
+    return User.name_exist(name)
+
+
+def display_user():
+    '''
+    Function that returns all the saved names
+    '''
+    return User.display_users()
