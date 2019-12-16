5c1a79cde68d17916e4978d6d4ad40ebfa88ea8a
Alice Mwihaki <alicemwihaki99@gmail.com>
2019-12-16 13:38:44 +0300

    created functions to create,save and delete user

--- run_user.py
+++ run_user.py
@@ -1,2 +1,21 @@
 #!/usr/bin/env python3.6
 from user import User
+
+def create_user(fname,lname,email):
+    '''
+    Function to create a new user
+    '''
+    new_user = User(fname,lname,email)
+    return new_user
+
+def save_users(user):
+    '''
+    Function to save user
+    '''
+    user.save_user()
+    
+def del_user(user):
+    '''
+    Function to delete a user
+    '''
+    user.delete_user()
